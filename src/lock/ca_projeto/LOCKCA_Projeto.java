f286832a956e391c754ddb25c28b0297391cc2f1
pivisoto <103545280+pivisoto@users.noreply.github.com>
2023-05-09 18:33:30 -0300

    Add files via upload
    Tela de login/cadastro e inicio da tela inicial

---
+++ src/lock/ca_projeto/LOCKCA_Projeto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lock.ca_projeto;
+
+/**
+ *
+ * @author pivis
+ */
+public class LOCKCA_Projeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new PaginaInicial().setVisible(true);
+    }
+    
+}
