9a18a9b68446585ac1d123969d4e8a6d988bd32a
Isabella Sanches <104502557+isaaSanches@users.noreply.github.com>
2023-05-19 11:30:02 -0300

    #35 Criação da pagina login e registro no html

---
+++ src/main/webapp/javascript.js
@@ -0,0 +1,25 @@
+const forms = document.querySelector(".forms"),
+  buttons = document.querySelectorAll(".registerButton"),
+  ShowHide = document.querySelectorAll(".eye-icon");
+ShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    let eyeIcon_senha_field =
+      eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+    eyeIcon_senha_field.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });
+});
+
+// document.getElementById("registerButton").onclick = teste();
+
+function toggleLogin() {
+  forms.classList.toggle("show-cadastrar");
+}
+
